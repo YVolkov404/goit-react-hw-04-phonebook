06360e031bb447790dd6f3adfa7629cb6c6e9c6e
Yuriy Volkov <sir.volkov@outlook.com>
2023-11-30 15:35:14 +0200

    upd : initial gh-pages commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+import { App } from './components/App';
+import { GlobalStyle } from 'components/GlobalStyle';
+import { ThemeProvider } from 'styled-components';
+
+/** Theme : color guide
+  ---> main color : background;
+  ---> second color { title, border, btnBackground, searchLabelUnderline, errorMsg, contactName, deleteContactBtnHoverAndFocus }
+  ---> third color { label, searchBorder, errorMsgUnderline, contactNumber, deleteContactBtn }
+  ---> hover & focus color : hoverAndFocus;
+**/
+
+const theme = {
+  main: 'black',
+  second: 'blanchedalmond',
+  third: 'aqua',
+  hovAndFoc: 'darkcyan',
+};
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+    <GlobalStyle />
   </React.StrictMode>
 );
